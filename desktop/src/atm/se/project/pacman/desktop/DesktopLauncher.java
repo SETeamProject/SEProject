e9778e8ab33694b59d624099cb88f73415760c0f
Livia Gusatu <livia.gusatu@gmail.com>
2015-01-18 00:11:27 +0200

    initial commit

---
+++ desktop/src/atm/se/project/pacman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package atm.se.project.pacman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import atm.se.project.pacman.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
